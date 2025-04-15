8dbf362f627a7330bd60308e6971557ca795795c
nandah4 <nyustira6@gmail.com>
2025-04-15 10:23:26 +0700

    add sorting

---
+++ src/ServiceClass.java
@@ -0,0 +1,45 @@
+
+public class ServiceClass {
+    TransaksiPembelian[] trs = new TransaksiPembelian[5];
+    int index = 0;
+
+    void tambahDataTransaksi(TransaksiPembelian trP) {
+        if (index < trs.length) {
+            trs[index] = trP;
+            index++;
+        } else {
+            System.out.println("Data Penuh!");
+        }
+    }
+
+    void searching(){
+        System.out.println("Cari Nama Pembeli: ");
+        String nama = "Santi"
+
+        int indexFound;
+
+        for(int i = 0; i < trs.length; i++){
+            if(nama.equalsIgnoreCase(trs[i].namaPembeli)) {
+                indexFound = i;
+                break;
+            } else {
+                indexFound = -1;
+            }
+        }
+
+        if(indexFound >= 0){
+            System.out.println("Data ditemukan di index - " + indexFound);
+        } else {
+            System.out.println("Data tidak ditemukan");
+        }
+    }
+
+    void sorting() {
+        for(int i = 0; i < trs.length - 1; i++){
+            for(int j = 1; j trs.length - i; j++) {
+
+            }
+        }
+    }
+
+}
