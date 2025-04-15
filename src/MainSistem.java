d2378a2394a127faca2a3d32e26f317c57157c07
nandah4 <nyustira6@gmail.com>
2025-04-15 09:51:04 +0700

    add data barang dummy

---
+++ src/MainSistem.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MainSistem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Barang brg1 = new Barang("Br001", "Leo Kripik", "Makanan", 10, 1000);
+        Barang brg2 = new Barang("Br002", "You C1000 Botol", "Minuman", 10, 7000);
+        Barang brg3 = new Barang("Br003", "Teh Pucuk Botol", "Minuman", 10, 4000);
+        Barang brg4 = new Barang("Br004", "Rinso Cair", "Sabun", 10, 7000);
+        Barang brg5 = new Barang("Br005", "Leo Kripik", "Sabun", 10, 1000);
+
+        while (true) {
+            System.out.println("================= TOKO Manasuka TOKO =================");
+        }
+    }
+}
